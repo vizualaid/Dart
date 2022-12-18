1f632effb0dabaece53345a73f83f05d1c76f751
vizualaid <isha.12011399@lpu.in>
2022-12-19 01:34:12 +0530

    Higher order function

--- 14 functionasFirstClassObject.dart
+++ 14 functionasFirstClassObject.dart
@@ -1,13 +1,29 @@
+//1 //function as first-class object
+//2 //Anonymous funtion or lambda
+//3 // Higher Order Function
+//any func that accepts a func as a parameter and
+// returns funtion as return value
 
-void getSum(List<int> numbers, Function result){
-var sum = 0;
-for(var i=0; i<numbers.length; i++){
-  sum+=numbers [i];}
+void getSum(List<int> numbers, Function result) {
+  var sum = 0;
+  for (var i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
   result(sum);
 }
+
+Function myfun() {
+  return () => print("This is a function call");
+}
+
 main() {
-var nums = [2,2,4,4];
-Function f1 
-(int sum) => print (sum);
-=
-getSum(nums, f1);
+  var nums = [100, 22, 13, 44, 94, 34];
+  // Function f1 = (int sum)=>print(sum);
+  Function f1 = (int sum) {
+    //1 function object
+    print("Sum is : "); //anonymous function as
+    print(sum); // it does not have any name
+  };
+  getSum(nums, f1);
+  myfun()();
+}
